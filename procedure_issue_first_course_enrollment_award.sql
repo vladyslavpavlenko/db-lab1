5a332fe18cabef7df0f9709a16923b700733925d
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-10-05 13:40:39 +0300

    feat(2): add issue_first_course_award procedure

--- procedure_issue_first_course_enrollment_award.sql
+++ procedure_issue_first_course_enrollment_award.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION issue_first_course_enrollment_award()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF (SELECT COUNT(*) FROM enrollments WHERE user_id = NEW.user_id) =  0 THEN
+    IF (SELECT COUNT(*) FROM enrollments WHERE user_id = NEW.user_id) = 0 THEN
         INSERT INTO user_awards (user_id, award_id, issued_at)
         VALUES (NEW.user_id, 1, CURRENT_TIMESTAMP);
     END IF;
